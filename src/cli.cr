f4206040f785f3421fde0a2d38260c96789d3705
Sijawusz Pur Rahnama <sija@sija.pl>
2020-05-20 05:19:27 +0200

    Enhance CLI arguments validation

--- src/cli.cr
+++ src/cli.cr
@@ -1,16 +1,23 @@
 require "./blurhash"
 
-unless ARGV.size == 3
-  abort "Usage: #{PROGRAM_NAME} x_components y_components image_path"
-end
+(ARGV.size == 3) ||
+  abort "Usage: #{PROGRAM_NAME} <x_components> <y_components> <image_path>"
 
 x_components, y_components, image_path = ARGV
 
+x_components = x_components.to_i? || abort "<x_components> argument must be a number"
+y_components = y_components.to_i? || abort "<y_components> argument must be a number"
+
+path = Path[image_path].expand(home: true)
+
+File.file?(path) ||
+  abort "<image_path> argument must point to a valid file"
+
 begin
   puts Blurhash.encode(
-    x_components: x_components.to_i,
-    y_components: y_components.to_i,
-    path: Path[image_path].expand(home: true)
+    x_components: x_components,
+    y_components: y_components,
+    path: path
   )
 rescue e : Exception
   abort e.message
